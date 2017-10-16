58685e2536633b8534d5dec19906e32c90f1fd16
Saurav Kumar Gupta <sauravgpt34@gmail.com>
2017-10-16 10:18:37 +0530

    Initial commit

---
+++ app/src/main/java/cheezycode/com/justmaths/MainActivity.java
@@ -0,0 +1,64 @@
+package cheezycode.com.justmaths;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnMathPlay, btnMathShare, btnMathRate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMathPlay = (ImageButton) findViewById(R.id.btnMathPlay);
+        btnMathShare= (ImageButton) findViewById(R.id.btnMathShare);
+        btnMathRate = (ImageButton) findViewById(R.id.btnMathRate);
+
+        btnMathPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                 *  Example of Explicit Intent
+                 *  When you click Play Button on the screen
+                 *  Game Activity will be started
+                 */
+
+                Intent i = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnMathShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                 *  Example of Implict Intent
+                 *  When you click Share Button on the screen
+                 *  Android will find the activities that allow to share messages.
+                 */
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Simple Maths - Fun way to learn Maths. Download here http://bit.ly/SimpleMathApp");
+                startActivity(intent);
+            }
+        });
+
+        btnMathRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                 *  Simple Toast Message To Display Message For Short Duration
+                 *  Link that to your app landing page.
+                 */
+                Toast.makeText(MainActivity.this,"App is still under construction.",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"Best Regards Saurav Gupta",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
